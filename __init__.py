10a3bb2e66f93a2e354567571249e460969c590b
meebox <meebox@gmail.com>
2023-07-17 10:39:31 +0800

    fix color pallet

--- __init__.py
+++ __init__.py
@@ -3,13 +3,23 @@ from colors import color
 
 def tokenizer(msg, model="gpt-3.5-turbo"):
     encoder = tiktoken.encoding_for_model(model)
-    color_pallet = ('blue', 'green', 'magenta', 'cyan')
+    color_pallet = (                
+        '#FFB347', # Pastel orange (淡橙色)
+        '#FFCC99', # Peach (桃色)
+        '#FFFF66', # Pastel yellow (淡黃色)
+        '#64fa64', # Tea green (茶綠色)
+        '#CCFFCC', # Aero blue (天青色)
+        '#CB99C9', # Thistle (薊色)
+        '#FFB6C1', # Light pink (淺粉紅色)
+        '#FFCCDD', # Pink lace (粉翠珠色)
+        '#CCCCFF'  # Lavender blue (薰衣草藍)
+    )
     idx = 0
     tokens = encoder.encode(msg)
     print(tokens)
     for token in tokens:
         print(color(encoder.decode([token]), 
-                    fg='white', 
+                    fg='black',
                     bg=color_pallet[idx]), 
               end='')
         idx = (idx + 1) % len(color_pallet)
